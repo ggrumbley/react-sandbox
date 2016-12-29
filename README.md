48d3ccbddddc4eb290385708fc2c2af331c47358
gary <ggrumbley@ucsd.edu>
2016-12-29 14:58:43 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# React Sandbox
+
+This repo is a place to experiment and build React components with full ES6 and JSX support.
+
+This project was bootstrapped with [Create React App](https://github.com/facebookincubator/create-react-app).
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/sample/App';
+// import Sheets from './components/Sheets';
+
+ReactDOM.render(
+  <App />,
+  // <Sheets headers={headers} initialData = {data} />,
+  document.getElementById('root')
+);
